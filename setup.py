063c252c37fe413e2e775950cbbb6f63d0715617
Jan Hochstrasser <jhochstrasse@student.ethz.ch>
2024-05-08 18:46:17 +0200

    actually add files for refactoring

---
+++ setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'SysLoadBench benchmarks functions for system load'
+LONG_DESCRIPTION = 'SysLoadBench benchmarks functions for system load including CPU and RAM utilization as well as timing'
+
+# Setting up
+setup(
+        name="sysloadbench", 
+        version=VERSION,
+        author="Jan Hochstrasser",
+        author_email="janhochstrasser@gmx.ch",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy>=1.26.0', 'psutil==5.9.8', 'py-cpuinfo==9.0.0', 'prettytable'],
+        
+        keywords=['python', 'benchmarking', 'system'],
+        classifiers= [
+            'Programming Language :: Python :: 3',
+            'Operating System :: Linux'
+        ]
+)
